9ab3ea3b53e4b288f003b1f7989fb6229139a8f5
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-08-23 07:02:54 -0400

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 import WelcomeMessage from './components/WelcomeMessage'
@@ -5,9 +8,6 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 
 function App() {
