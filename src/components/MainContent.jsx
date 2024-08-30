8588879b5a44206ff6cdfaa5847426ff5c73da3c
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-08-30 10:41:41 -0400

    update styling after assignment 1

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-      <main>
+      <main style={{ backgroundColor: "white" , color: '#242424', textAlign: 'center' }}>
           <p>I love to visit New York, Paris, and Tokyo.</p>
       </main>
   );
